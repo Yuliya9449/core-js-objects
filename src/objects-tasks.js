ed7ab1336c24c8a6d4ebc89e33531cef4adf44b8
Yuliya Kotova <yulya9449@yandex.ru>
2024-03-02 02:23:32 +0300

    feat: add solution shallowCopy task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,11 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+
+//! 1
+function shallowCopy(obj) {
+  const newObj = {};
+  return Object.assign(newObj, obj);
 }
 
 /**
@@ -32,6 +35,8 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
+
+//! 2
 function mergeObjects(/* objects */) {
   throw new Error('Not implemented');
 }
