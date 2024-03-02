316e53137fa321ca184fcb484ada349139ff7d41
Yuliya Kotova <yulya9449@yandex.ru>
2024-03-02 03:33:56 +0300

    feat: add solution makeImmutable task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -125,8 +125,8 @@ function isEmptyObject(obj) {
  */
 
 //! 6
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -139,6 +139,8 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
+
+//! 7
 function makeWord(/* lettersObject */) {
   throw new Error('Not implemented');
 }
